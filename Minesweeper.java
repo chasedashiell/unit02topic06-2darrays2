dbf8f2a5eb7c067405dd2d4a2916a8c8d2ce21bb
chasedashiell <dashchax@gmail.com>
2023-02-24 14:26:31 -0600

    bug in minesweeper

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,5 +1,94 @@
+import java.util.Arrays;
+
+import javax.xml.transform.SourceLocator;
+
 public class Minesweeper {
+
+    public static void print2DInt(int arr[][]){
+      for (int[] row : arr){
+          System.out.println(Arrays.toString(row));
+    }
+  }
+  public static void print2DBool(boolean arr[][]){
+    for (boolean[] row : arr){
+        System.out.println(Arrays.toString(row));
+  }
+}
+  
+    public static boolean[][] makeMinesweeperBoard(int numRows, int numCols, double p){
+        boolean[][] solution = new boolean[numRows][numCols];
+        for (int row = 0; row < numRows; row++){
+            for (int col = 0; col < numCols; col++){
+                double rand = Math.random();
+                if (p > rand){
+                    solution[row][col] = true;
+                } else{
+                    solution[row][col] = false;
+                }
+            }
+        }
+        return solution;
+    }
+
+    public static void printMinesweeperBoard(boolean[][] board){
+        for (boolean[] row : board){
+            System.out.println();
+            for (boolean tf : row){
+                if (tf){
+                    System.out.print('*');
+                }else{
+                    System.out.print('.');
+                }
+            }
+        }
+    }
+
+    public static int[][] makeNeighborBoard(boolean[][] board){
+        int[][] solution = new int[board.length][board[0].length];
+        for(int row = 0; row < board.length; row++){
+            for (int col = 0; col < board[0].length; col++){
+                if (board[row][col]){
+                    solution[row][col] = -1;
+                    if(row != board.length-1 && solution[row+1][col] != 1){
+                        solution[row+1][col]++;
+                    }
+                    if(row != 0 && solution[row-1][col] != 1){
+                        solution[row-1][col]++;
+                    }
+                    if(col != board[0].length-1 && row != board.length-1 && solution[row+1][col+1] != 1 ){
+                        solution[row+1][col+1]++;
+                    }
+                    if(row!=0 && col!=0 && solution[row-1][col-1] != 1){
+                        solution[row-1][col-1]++;
+                    }
+                    if(row != board.length-1 && col != 0 && solution[row+1][col-1] != 1){
+                        solution[row+1][col-1]++;
+                    }
+                    if(col != board[0].length-1 && solution[row][col+1] != 1){
+                        solution[row][col+1]++;
+                    }
+                    if(col != 0 && solution[row][col-1] != 1){
+                        solution[row][col-1]++;
+                    }
+                    if(row != 0 && col != board[0].length-1 && solution[row-1][col+1] != 1){
+                        solution[row-1][col+1]++;
+                    }
+
+                }
+            }
+        }
+        return solution;
+    }
     public static void main(String[] args) {
-        
+        boolean[][] arr = makeMinesweeperBoard(3, 3, 0.15);
+        print2DBool(arr);
+        print2DInt(makeNeighborBoard(arr));
+
+        // [false, true, false]
+        // [false, false, true]
+        // [false, false, true]
+        // [1, 0, 1]
+        // [1, 1, 0]
+        // [0, 1, -1]
     }
 }
--- SelfAvoidingWalk.java
+++ SelfAvoidingWalk.java
@@ -1,39 +1,88 @@
+import java.util.Arrays;
+
 public class SelfAvoidingWalk {
 
-    private static boolean walk(int xlen, int ylen){
-        //true == made it out, false == did not make it out
+    private static int[] walk(int xlen, int ylen){
+        //positive == made it out, negative == did not make it out
 
         //setting the position to the middle of the board
         int positionX = xlen/2;
         int positionY = ylen/2;
         boolean[][] grid = new boolean[xlen][ylen];
-        
-
+        int length = 0;
+        int[] solution = new int[3];
+        int maxX = 0;
+        int maxY = 0;
         //gameloop
         while(positionX >= 0 && positionX < xlen && positionY >= 0 && positionY < ylen && grid[positionX][positionY] == false){
-            
             //moving the character
             grid[positionX][positionY] = true;
             double randNum = Math.random();
             if(randNum <= 0.25){
                 positionX += 1;
+                length++;
             } else if(randNum <= 0.5){
                 positionX -= 1;
+                length++;
             } else if(randNum <= 0.75){
                 positionY -= 1;
+                length++;
             } else {
                 positionY += 1;
+                length++;
             }
+
+            if(positionX > maxX){
+                maxX = positionX;
+            }
+
+            if(positionY > maxY){
+                maxY = positionY;
+            }
+
         }
         
-        //finding weither he escaped or not
+        //finding weither it escaped or not
         if(positionX < 0 || positionX >= xlen || positionX < 0 || positionY >= ylen){
-                return true;
+                solution[0] = length;
+            } else{
+        solution[0] = -length;
             }
-        return false;
+        solution[1] = maxX;
+
+        solution[2] = maxY;
+
+        return solution;
+    }
+
+    public static void printPathLengths(int size, int nTrials) {
+        int numEscaped = 0;
+        int lengthTotal = 0;
+
+        for(int i = 0; i <= nTrials; i++){
+            int[] arr = walk(size, size);
+            int solution = arr[0];
+            int length = Math.abs(solution);
+            lengthTotal += length;
+            if (solution > 0){
+                numEscaped++;
+            }
+        }
+        System.out.println("Average length: " + lengthTotal / nTrials);
+        System.out.println("Probability of escaping: " + (double) numEscaped / nTrials);
+    }
+
+    public static double deadEndRectangleArea(int size, int nTrials){
+        double total = 0;
+        for (int i = 0; i < nTrials +1; i++){
+            int[] arr = walk(size, size);
+            total += (double) arr[1] * arr[2];
+        }
+        return total / nTrials;
     }
 
     public static void main(String[] args) {
-        System.out.println(walk(6,6));
+        printPathLengths(8, 1000);
+        System.out.println( deadEndRectangleArea(8, 1000));
     }
 }
